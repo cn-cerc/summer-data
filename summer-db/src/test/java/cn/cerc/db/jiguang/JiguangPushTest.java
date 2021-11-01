05b6ed42b09ebf3b5f838489ff3bc367651816d4
itjun <l1091462907@gmail.com>
2021-11-01 06:15:25 +0000

    !58 升级到java11

--- summer-db/src/test/java/cn/cerc/db/jiguang/JiguangPushTest.java
+++ summer-db/src/test/java/cn/cerc/db/jiguang/JiguangPushTest.java
@@ -35,11 +35,11 @@ public class JiguangPushTest {
 
         // 发送给指定的设备Id
 //        push.send(ClientType.Android, "n_f526c72cf13474d7");// itjun-xiaomi-mix2
-        push.send(ClientType.Android, "n_d4ffa59ea6d1d9f3");// joylee-xiaomi-mix2
+//        push.send(ClientType.Android, "n_d4ffa59ea6d1d9f3");// joylee-xiaomi-mix2
 //        push.send(ClientType.Android, "n_a86ddf4df465a83f");// weibo-xiaomi-5c
 //        push.send(ClientType.IOS, "i_82D7A281FC37497F810085A6D7EFB1B2");// ly-iPhone-SE2
 //        push.send(ClientType.IOS, "i_A1688A8910B04499ABC64B035B559921");// wsg-iPhone-xr
-//        push.send(ClientType.IOS, "i_82D7A281FC37497F810085A6D7EFB1B2");// itjun-iPhone-SE2
+        push.send(ClientType.IOS, "i_0FE2031581984EB5B38970D236F78043");// itjun-iPhone-SE2，关闭生产环境 setApnsProduction
 //        push.send(ClientType.IOS, "i_87E2C6FB4D2347F49FC17CD564B07AEE", sound);// HuangRongjun-iPhone
 
         // 发送给指定的设备类型
