0d4ebc83a5ff7272219e6281ada139f8b6f3868a
mulingyuchen <1040826451@qq.com>
2021-12-06 14:53:10 +0800

    Update MssqlOperator.java

--- summer-db/src/main/java/cn/cerc/db/mssql/MssqlOperator.java
+++ summer-db/src/main/java/cn/cerc/db/mssql/MssqlOperator.java
@@ -2,7 +2,9 @@ package cn.cerc.db.mssql;
 
 import java.math.BigInteger;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import cn.cerc.core.DataRow;
 import cn.cerc.core.SqlText;
@@ -48,8 +50,47 @@ public class MssqlOperator extends SqlOperator {
     }
 
     @Override
-    protected BigInteger findAutoUid(Connection connection) {
-        return null;
+    protected BigInteger findAutoUid(Connection conn) {
+        BigInteger result = null;
+        String sql = "SELECT SCOPE_IDENTITY()";
+        Statement stmt = null;
+        ResultSet rs = null;
+        try {
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
+            if (rs.next()) {
+                Object obj = rs.getObject(1);
+                if (obj instanceof BigInteger) {
+                    result = (BigInteger) obj;
+                } else {
+                    result = BigInteger.valueOf(rs.getInt(1));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e.getMessage());
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new RuntimeException(e.getMessage());
+            }
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new RuntimeException(e.getMessage());
+            }
+        }
+        if (result == null) {
+            throw new RuntimeException("未获取UID");
+        }
+        return result;
     }
 
     @Override
