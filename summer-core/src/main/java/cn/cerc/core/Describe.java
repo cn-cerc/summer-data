761b9369ca85c7c693f7445b1d4716376fff8b88
张弓 <sz9214e@qq.com>
2021-12-07 11:09:38 +0800

    Update Describe.java

--- summer-core/src/main/java/cn/cerc/core/Describe.java
+++ summer-core/src/main/java/cn/cerc/core/Describe.java
@@ -1,5 +1,13 @@
 package cn.cerc.core;
 
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Target({ ElementType.TYPE, ElementType.METHOD, ElementType.FIELD })
+@Retention(RUNTIME)
 public @interface Describe {
 
     String name();
