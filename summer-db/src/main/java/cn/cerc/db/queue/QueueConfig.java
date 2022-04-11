ae5486af32e9ccdc0eaaeed4fbfbfdc00b622f87
IT君 <l1091462907@gmail.com>
2022-04-11 10:29:37 +0800

    Update QueueConfig.java

--- summer-db/src/main/java/cn/cerc/db/queue/QueueConfig.java
+++ summer-db/src/main/java/cn/cerc/db/queue/QueueConfig.java
@@ -1,36 +1,53 @@
 package cn.cerc.db.queue;
 
+import cn.cerc.db.core.ClassConfig;
+import cn.cerc.db.core.Utils;
+
 /**
  * 阿里云消息队列
  */
 public class QueueConfig {
 
+    private static final ClassConfig config = new ClassConfig(QueueConfig.class, null);
+
     /**
      * 系统消息
      **/
     public static final String getMessageQueue() {
-        return "message";
+        String queue = config.getProperty("application.queue.message");
+        if (Utils.isEmpty(queue))
+            throw new RuntimeException("the queue key application.queue.message is empty");
+        return queue;
     }
 
     /**
      * 回算队列
      **/
     public static final String getSummerQueue() {
-        return "summer";
+        String queue = config.getProperty("application.queue.summer");
+        if (Utils.isEmpty(queue))
+            throw new RuntimeException("the queue key application.queue.summer is empty");
+        return queue;
     }
 
     /**
      * 资料同步
      **/
     public static final String getMaterialQueue() {
-        return "material";
+        String queue = config.getProperty("application.queue.material");
+        if (Utils.isEmpty(queue))
+            throw new RuntimeException("the queue key application.queue.material is empty");
+        return queue;
     }
 
     /**
      * 全文检索
      */
     public static final String getElasticsearchQueue() {
-        return "elasticsearch";
+        String queue = config.getProperty("application.queue.elasticsearch");
+        if (Utils.isEmpty(queue))
+            throw new RuntimeException("the queue key application.queue.elasticsearch is empty");
+        return queue;
     }
 
 }
