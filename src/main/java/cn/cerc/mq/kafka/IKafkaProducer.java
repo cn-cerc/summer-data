bec19ca25d2cba05b334f0f3f9fa0ea0a58bc13f
LeeDante <314670899@qq.com>
2022-08-03 10:45:40 +0800

    Update IKafkaProducer.java

--- src/main/java/cn/cerc/mq/kafka/IKafkaProducer.java
+++ src/main/java/cn/cerc/mq/kafka/IKafkaProducer.java
@@ -47,24 +47,40 @@ public abstract class IKafkaProducer implements AutoCloseable {
     }
 
     /**
+     * 发送带key的消息并传入回调函数，回调也是异步
+     * 
+     * @param partition
+     * @param key
+     * @param value
+     * @param callback
+     * @return
+     */
+    public Future<RecordMetadata> send(Integer partition, String key, String value, Callback callback) {
+        return producer.send(new ProducerRecord<String, String>(topic(), partition, key, value), callback);
+    }
+
+    /**
+     * 发送不带key的消息并传入回调函数，回调也是异步
+     * 
+     * @param partition
+     * @param value
+     * @param callback
+     * @return
+     */
+    public Future<RecordMetadata> send(Integer partition, String value, Callback callback) {
+        return send(partition, null, value, callback);
+    }
+
+    /**
      * 发送带key的消息
      * 
+     * @param partition
      * @param key
      * @param value
      * @return
      */
-    public Future<RecordMetadata> send(String key, String value) {
-        return producer.send(new ProducerRecord<String, String>(topic(), key, value));
-    }
-
-    /**
-     * 发送不带key的消息
-     * 
-     * @param value
-     * @return
-     */
-    public Future<RecordMetadata> send(String value) {
-        return producer.send(new ProducerRecord<String, String>(topic(), value));
+    public Future<RecordMetadata> send(Integer partition, String key, String value) {
+        return send(partition, key, value, null);
     }
 
     /**
@@ -76,18 +92,50 @@ public abstract class IKafkaProducer implements AutoCloseable {
      * @return
      */
     public Future<RecordMetadata> send(String key, String value, Callback callback) {
-        return producer.send(new ProducerRecord<String, String>(topic(), key, value), callback);
+        return send(null, key, value, callback);
     }
 
     /**
-     * 发送不带key的消息并传入回调函数，回调也是异步
+     * 发送带key的消息
      * 
+     * @param key
      * @param value
+     * @return
+     */
+    public Future<RecordMetadata> send(String key, String value) {
+        return send(null, key, value, null);
+    }
+
+    /**
+     * 发送不带key的消息
+     * 
+     * @param partition
+     * @param value
+     * @return
+     */
+    public Future<RecordMetadata> send(Integer partition, String value) {
+        return send(null, null, value, null);
+    }
+
+    /**
+     * 发送不带key的消息
+     * 
      * @param callback
+     * @param value
      * @return
      */
     public Future<RecordMetadata> send(String value, Callback callback) {
-        return producer.send(new ProducerRecord<String, String>(topic(), value), callback);
+        return send(null, null, value, callback);
+    }
+
+    /**
+     * 发送不带key的消息
+     * 
+     * @param value
+     * @return
+     */
+    public Future<RecordMetadata> send(String value) {
+        return send(null, null, value, null);
     }
 
     @Override
