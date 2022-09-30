ee1ad40f61a5d220bf4aceb96a807d52e2733c38
张建红 <sz9214e@qq.com>
2022-09-30 06:42:30 +0000

    !144 增加新的md5函数

--- src/main/java/cn/cerc/db/core/MD5.java
+++ src/main/java/cn/cerc/db/core/MD5.java
@@ -25,4 +25,25 @@ public class MD5 {
             throw new RuntimeException(e.getMessage());
         }
     }
+
+    public final static String get(byte[] str) {
+        MessageDigest md5;
+        try {
+            md5 = MessageDigest.getInstance("MD5");
+
+            byte[] bs = md5.digest(str);
+            StringBuilder sb = new StringBuilder(40);
+            for (byte x : bs) {
+                if ((x & 0xff) >> 4 == 0) {
+                    sb.append("0").append(Integer.toHexString(x & 0xff));
+                } else {
+                    sb.append(Integer.toHexString(x & 0xff));
+                }
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e.getMessage());
+        }
+    }
 }
