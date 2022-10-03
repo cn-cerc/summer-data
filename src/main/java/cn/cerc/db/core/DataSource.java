c0c1894efbd6b8a2242e3c755eec8917096d761b
张弓 <sz9214e@qq.com>
2022-10-03 10:22:51 +0800

    增加函数说明

--- src/main/java/cn/cerc/db/core/DataSource.java
+++ src/main/java/cn/cerc/db/core/DataSource.java
@@ -1,16 +1,27 @@
 package cn.cerc.db.core;
 
 public interface DataSource {
-    
+
+    /**
+     * @return 返回 dataSet，可为null
+     */
     default DataSet dataSet() {
         return null;
     }
 
+    /**
+     * 
+     * @return 返回dataRow，若dataSet()不为空，则为dataSet.current()
+     */
     default DataRow current() {
         var dataSet = dataSet();
         return dataSet != null ? dataSet.current() : null;
     }
 
+    /**
+     * 
+     * @return 返回数据是否只读，默认值为 true
+     */
     default boolean readonly() {
         var current = current();
         return current != null ? current().readonly() : true;
