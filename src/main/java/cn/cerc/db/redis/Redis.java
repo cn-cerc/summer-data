eb91984794cc35598392c40729552af8695fe489
张建红 <sz9214e@qq.com>
2022-10-09 07:53:49 +0000

    !147 改进redis类，支持直接创建，提升可读性 * 改进redis类，支持直接创建

--- src/main/java/cn/cerc/db/redis/Redis.java
+++ src/main/java/cn/cerc/db/redis/Redis.java
@@ -1,36 +1,168 @@
 package cn.cerc.db.redis;
 
-import redis.clients.jedis.Jedis;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-public class Redis {
+import redis.clients.jedis.JedisPubSub;
 
-    public static String get(String key) {
-        try (Jedis jedis = JedisFactory.getJedis()) {
-            return jedis != null ? jedis.get(key) : null;
+public class Redis implements AutoCloseable {
+
+    private redis.clients.jedis.Jedis jedis;
+
+    public Redis() {
+        this.jedis = JedisFactory.getJedis();
+    }
+
+    public Redis(String configId) {
+        this.jedis = JedisFactory.getJedis(configId);
+    }
+
+    @Override
+    public void close() {
+        if (jedis != null) {
+            jedis.close();
+            jedis = null;
         }
     }
 
-    public static void set(String key, Object value) {
-        set(key, value, 3600);
+    public void expire(String key, final int seconds) {
+        if (jedis != null)
+            jedis.expire(key, seconds);
     }
 
-    public static void set(String key, Object value, int expires) {
-        try (Jedis jedis = JedisFactory.getJedis()) {
-            if (jedis != null) {
-                if (value instanceof String) {
-                    jedis.set(key, (String) value);
-                } else {
-                    jedis.set(key, value.toString());
-                }
-                jedis.expire(key, expires);
-            }
+    public void hdel(final String key, final String... fields) {
+        if (jedis != null)
+            jedis.hdel(key, fields);
+    }
+
+    public String hget(String key, String field) {
+        return jedis != null ? jedis.hget(key, field) : null;
+    }
+
+    public Map<String, String> hgetAll(String key) {
+        if (jedis != null)
+            return jedis.hgetAll(key);
+        else
+            return new HashMap<String, String>();
+    }
+
+    public void hset(String key, String field, String value) {
+        if (jedis != null)
+            jedis.hset(key, field, value);
+    }
+
+    public void del(String key) {
+        if (jedis != null)
+            jedis.del(key);
+    }
+
+    public Set<String> hkeys(String key) {
+        if (jedis != null)
+            return jedis.hkeys(key);
+        else
+            return new HashSet<String>();
+    }
+
+    public void subscribe(final JedisPubSub jedisPubSub, final String... channels) {
+        if (jedis != null)
+            jedis.subscribe(jedisPubSub, channels);
+    }
+
+    public String get(String key) {
+        if (jedis != null)
+            return jedis.get(key);
+        else
+            return null;
+    }
+
+    public String getSet(String key, String value) {
+        if (jedis != null)
+            return jedis.getSet(key, value);
+        else
+            return null;
+    }
+
+    public Set<String> keys(String key) {
+        if (jedis != null)
+            return jedis.keys(key);
+        else
+            return new HashSet<String>();
+    }
+
+    public void rpush(final String key, final String... strings) {
+        if (jedis != null)
+            jedis.rpush(key, strings);
+    }
+
+    public void lpush(final String key, final String... strings) {
+        if (jedis != null)
+            jedis.lpush(key, strings);
+    }
+
+    public void publish(final String channel, final String message) {
+        if (jedis != null)
+            jedis.publish(channel, message);
+    }
+
+    public String rpop(String key) {
+        if (jedis != null)
+            return jedis.rpop(key);
+        else
+            return null;
+    }
+
+    public String scriptLoad(String luaScript) {
+        if (jedis != null)
+            return jedis.scriptLoad(luaScript);
+        else
+            return null;
+    }
+
+    public void evalsha(String sha1, List<String> keys, List<String> args) {
+        if (jedis != null)
+            jedis.evalsha(sha1, keys, args);
+    }
+
+    public void set(String key, String value) {
+        if (jedis != null)
+            jedis.set(key, value);
+    }
+
+    public void setex(final String key, final int seconds, final String value) {
+        if (jedis != null)
+            jedis.setex(key, seconds, value);
+    }
+
+    public long setnx(String key, String value) {
+        if (jedis != null)
+            return jedis.setnx(key, value);
+        else
+            return 0;
+    }
+
+    public static String getValue(String key) {
+        try (Redis redis = new Redis()) {
+            return redis.get(key);
         }
+    }
+
+    public static void setValue(String key, String value, int seconds) {
+        try (Redis redis = new Redis()) {
+            redis.setex(key, seconds, value);
+        }
+    }
+
+    public static void setValue(String key, String value) {
+        setValue(key, value, 3600);
     }
 
     public static void delete(String key) {
-        try (Jedis jedis = JedisFactory.getJedis()) {
-            if (jedis != null)
-                jedis.del(key);
+        try (Redis redis = new Redis()) {
+            redis.del(key);
         }
     }
+
 }
