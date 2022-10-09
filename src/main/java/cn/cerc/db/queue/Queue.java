b2c1747f5f5585e121361bdf4ec7b198dd08a33d
张弓 <sz9214e@qq.com>
2022-10-09 22:45:00 +0800

    清理未使用的代码

--- src/main/java/cn/cerc/db/queue/Queue.java
+++ src/main/java/cn/cerc/db/queue/Queue.java
@@ -1,32 +1,28 @@
 package cn.cerc.db.queue;
 
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.aliyun.mns.client.AsyncCallback;
 import com.aliyun.mns.client.CloudQueue;
 import com.aliyun.mns.common.ClientException;
 import com.aliyun.mns.model.Message;
 
-import cn.cerc.db.core.Datetime;
-
-public class Queue implements AsyncCallback<List<Message>>, AutoCloseable {
+public class Queue implements AutoCloseable {
     private static final Logger log = LoggerFactory.getLogger(Queue.class);
 
-    private CloudQueue client;
-    private QueueServer server = new QueueServer();
+    private QueueServer server;
+    private CloudQueue queue;
 
     public Queue(String queueId) {
         super();
-        this.client = server.createQueue(queueId);
+        this.server = new QueueServer();
+        this.queue = server.createQueue(queueId);
     }
 
     public Message read() {
         Message message = null;
         try {
-            message = client.popMessage();
+            message = queue.popMessage();
             if (message != null) {
                 log.debug("messageBody：{}", message.getMessageBodyAsString());
                 log.debug("messageId：{}", message.getMessageId());
@@ -45,75 +41,19 @@ public class Queue implements AsyncCallback<List<Message>>, AutoCloseable {
     public void send(String content) {
         Message message = new Message();
         message.setMessageBody(content);
-        client.putMessage(message);
+        queue.putMessage(message);
     }
 
     public void delete(Message message) {
         if (message != null)
-            client.deleteMessage(message.getReceiptHandle());
+            queue.deleteMessage(message.getReceiptHandle());
         return;
-    }
-
-    public void recevie() {
-        client.asyncBatchPopMessage(10, 5, this);
-    }
-
-    @Override
-    public void onSuccess(List<Message> result) {
-        for (var message : result) {
-            System.out.println("onSuccess: " + message.getMessageBody());
-            this.delete(message);
-        }
-    }
-
-    @Override
-    public void onFail(Exception ex) {
-        ex.printStackTrace();
-        System.out.println("出错了: " + ex.getMessage());
     }
 
     @Override
     public void close() {
         server.close();
         server = null;
-    }
-
-    public CloudQueue client() {
-        return this.client;
-    }
-
-    public static void main(String[] args) {
-        Queue queue = new Queue("test");
-//        for (int i = 1; i < 4; i++)
-//            queue.send("val" + i);
-        System.out.println("clear");
-        Message msg = null;
-        do {
-            msg = queue.read();
-            if (msg != null) {
-                System.out.println(msg.getMessageBody());
-                System.out.println(msg.getNextVisibleTime());
-                queue.delete(msg);
-            }
-        } while (msg != null);
-        System.out.println("start recevie: " + new Datetime());
-        System.out.println("start send: " + new Datetime());
-        for (int i = 0; i < 5; i++) {
-            try {
-                queue.send(new Datetime().toString());
-                Thread.sleep(1000);
-                queue.recevie();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        queue.close();
-        System.out.println("end: " + new Datetime());
     }
 
 }
