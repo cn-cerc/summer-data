32b6fb19f2c76311b9b41df9099ad85a4aa6a4f5
张弓 <sz9214e@qq.com>
2022-10-29 21:10:45 +0800

    Update QueueQuery.java

--- src/main/java/cn/cerc/db/queue/QueueQuery.java
+++ src/main/java/cn/cerc/db/queue/QueueQuery.java
@@ -3,7 +3,10 @@ package cn.cerc.db.queue;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 
+import org.apache.rocketmq.client.apis.ClientException;
 import org.apache.rocketmq.client.apis.message.MessageView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import cn.cerc.db.core.DataSet;
 import cn.cerc.db.core.DataSetGson;
@@ -11,6 +14,7 @@ import cn.cerc.db.core.Utils;
 
 public class QueueQuery extends DataSet {
     private static final long serialVersionUID = 7781788221337787366L;
+    private static final Logger log = LoggerFactory.getLogger(QueueQuery.class);
 
     private transient QueueConsumer consumer;
     private transient MessageView message;
@@ -34,9 +38,12 @@ public class QueueQuery extends DataSet {
         return message != null;
     }
 
-    public String save(String json) {
-        try (var queue = QueueProducer(this.topic, QueueConfig.tag)) {
-            return QueueServer.append(json, Duration.ZERO);
+    public void save(String json) {
+        try (var queue = new QueueProducer(this.topic, QueueConfig.tag)) {
+            queue.append(json, Duration.ZERO);
+        } catch (ClientException e) {
+            log.error(e.getMessage());
+            e.printStackTrace();
         }
     }
 
