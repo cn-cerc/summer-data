d4ac63c73c618b53d1464efc206e502fdce01276
张弓 <sz9214e@qq.com>
2022-10-31 12:20:40 +0800

    Update AbstractQueue.java

--- src/main/java/cn/cerc/db/queue/AbstractQueue.java
+++ src/main/java/cn/cerc/db/queue/AbstractQueue.java
@@ -6,13 +6,16 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 import org.apache.rocketmq.client.apis.ClientException;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import cn.cerc.db.core.ServerConfig;
 import cn.cerc.db.zk.ZkConfig;
 
-public abstract class AbstractQueue implements OnStringMessage, ServletContextListener {
+public abstract class AbstractQueue implements OnStringMessage, ServletContextListener, Watcher {
     private static final Logger log = LoggerFactory.getLogger(AbstractQueue.class);
     private static ZkConfig config;
     private QueueConsumer consumer;
@@ -64,26 +67,49 @@ public abstract class AbstractQueue implements OnStringMessage, ServletContextLi
         if (consumer != null)
             return;
 
-        if (config == null)
-            config = new ZkConfig(String.format("/app/%s/task", ServerConfig.getAppName()));
-        config.setTempNode(this.getGroupId(), "running");
+        try {
+            ZkConfig status = new ZkConfig(String.format("/app/%s", ServerConfig.getAppName()));
+            var stat = status.client().exists(config.path() + "/status", this);
+            if (stat == null) {
+                status.setValue("status", "running");
+                stat = status.client().exists(config.path() + "/status", this);
+                if (stat == null) {
+                    log.warn("配置有误，无法启动消息队列");
+                    return;
+                }
+            }
+        } catch (KeeperException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        config().setTempNode(this.getGroupId(), "running");
 
         log.info("{} 启动了消息推送服务", this.getTopic());
         consumer = QueueConsumer.getConsumer(this.getTopic(), this.getTag());
         consumer.createQueueGroup(this);
     }
 
+    @Override
+    public void process(WatchedEvent event) {
+        if (event.getType() == Watcher.Event.EventType.DataWatchRemoved) {
+            log.info("主机被移除");
+            this.stopService();
+        }
+    }
+
     public void stopService() {
         if (consumer == null)
             return;
-
-        if (config == null)
-            config = new ZkConfig(String.format("/app/%s/task", ServerConfig.getAppName()));
-        config.delete(this.getGroupId());
-
+        config().delete(this.getGroupId());
         log.info("{} 关闭了消息推送服务", this.getTopic());
         consumer.close();
         consumer = null;
+    }
+
+    private ZkConfig config() {
+        if (config == null)
+            config = new ZkConfig(String.format("/app/%s/task", ServerConfig.getAppName()));
+        return config;
     }
 
     protected String sendMessage(String data) {
