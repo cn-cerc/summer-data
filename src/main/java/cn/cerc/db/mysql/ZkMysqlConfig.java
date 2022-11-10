8953922007c694925a31a3e7959d237d59f75cb3
张弓 <sz9214e@qq.com>
2022-11-10 20:21:26 +0800

    update

--- src/main/java/cn/cerc/db/mysql/ZkMysqlConfig.java
+++ src/main/java/cn/cerc/db/mysql/ZkMysqlConfig.java
@@ -1,57 +1,204 @@
 package cn.cerc.db.mysql;
 
+import java.beans.PropertyVetoException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import cn.cerc.db.core.ServerConfig;
+import cn.cerc.db.core.Utils;
 import cn.cerc.db.zk.ZkNode;
 
 public class ZkMysqlConfig {
-
+    private static final Logger log = LoggerFactory.getLogger(ZkMysqlConfig.class);
+    // mysql驱动
+    public static final String JdbcDriver;
+    private static ZkMysqlConfig instanceMaster;
+    private static ZkMysqlConfig instanceSalve;
+    private static ServerConfig config;
     private ZkNode node = ZkNode.get();
+    private String savleFlag = "";
+
+    static {
+        config = ServerConfig.getInstance();
+        JdbcDriver = config.getProperty("spring.datasource.driver-class-name", "com.mysql.cj.jdbc.Driver");
+    }
+
+    public synchronized static ZkMysqlConfig getMaster() {
+        if (instanceMaster == null)
+            instanceMaster = new ZkMysqlConfig(true);
+        return instanceMaster;
+    }
+
+    public synchronized static ZkMysqlConfig getSlave() {
+        if (instanceSalve == null)
+            instanceSalve = new ZkMysqlConfig(false);
+        return instanceSalve;
+    }
+
+    private ZkMysqlConfig(boolean isMaster) {
+        if (isMaster) {
+            instanceMaster = this;
+            savleFlag = "";
+        } else {
+            instanceSalve = this;
+            savleFlag = ".slave";
+        }
+    }
 
     public String site() {
-        return node.getString(getNodePath("site"), "127.0.0.1:3306");
+        return node.getString(getNodePath("site"), config.getProperty("rds.site" + savleFlag, "127.0.0.1:3306"));
     }
 
     public String database() {
-        return node.getString(getNodePath("database"), "appdb");
+        return node.getString(getNodePath("database"), config.getProperty("rds.database" + savleFlag, "appdb"));
     }
 
     public String username() {
-        return node.getString(getNodePath("username"), "appdb_user");
+        return node.getString(getNodePath("username"), config.getProperty("rds.username" + savleFlag, "appdb_user"));
     }
 
     public String password() {
-        return node.getString(getNodePath("password"), "appdb_password");
+        return node.getString(getNodePath("password"),
+                config.getProperty("rds.password" + savleFlag, "appdb_password"));
     }
 
     public String serverTimezone() {
         return node.getString(getNodePath("serverTimezone"), "Asia/Shanghai");
     }
 
+    /**
+     * 连接池最大连接数，默认为0（不启用），建议设置为最大并发请求数量
+     * 
+     * @return maxPoolSize
+     */
     public int maxPoolSize() {
-        return node.getInt(getNodePath("MaxPoolSize"), 0);
+        return node.getInt(getNodePath("MaxPoolSize"), config.getInt("rds.MaxPoolSize", 0));
     }
 
+    /**
+     * 连接池最小连接数，默认为9，即CPU核心数*2+1
+     * 
+     * @return minPoolSize
+     */
     public int minPoolSize() {
-        return node.getInt(getNodePath("MinPoolSize"), 9);
+        return node.getInt(getNodePath("MinPoolSize"), config.getInt("rds.MinPoolSize", 9));
     }
 
+    /**
+     * 连接池在建立时即初始化的连接数量
+     * 
+     * @return initialPoolSize
+     */
     public int initialPoolSize() {
-        return node.getInt(getNodePath("InitialPoolSize"), 3);
+        return node.getInt(getNodePath("InitialPoolSize"), config.getInt("rds.InitialPoolSize", 3));
     }
 
+    /**
+     * 设置创建连接超时时间，单位为毫秒，默认为0.5秒，此值建议设置为不良体验值（当前为超出1秒即警告）的一半
+     * 
+     * @return checkoutTimeout
+     */
     public int checkoutTimeout() {
-        return node.getInt(getNodePath("CheckoutTimeout"), 500);
+        return node.getInt(getNodePath("CheckoutTimeout"), config.getInt("rds.CheckoutTimeout", 500));
     }
 
+    /**
+     * 检查连接池中所有连接的空闲，单位为秒。注意MySQL空闲超过8小时连接自动关闭） 默认为空闲2小时即自动断开，建议其值为
+     * tomcat.session的生存时长(一般设置为120分钟) 加10分钟，即130 * 60 = 7800
+     * 
+     * @return maxIdleTime
+     */
     public int maxIdleTime() {
-        return node.getInt(getNodePath("MaxIdleTime"), 7800);
+        return node.getInt(getNodePath("MaxIdleTime"), config.getInt("rds.MaxIdleTime", 7800));
     }
 
+    /**
+     * 检查连接池中所有空闲连接的间隔时间，单位为秒。默认为9秒，其值应比 mysql 的connect_timeout默认为10秒少1秒，即9秒
+     * 
+     * @return idleConnectionTestPeriod
+     */
     public int idleConnectionTestPeriod() {
-        return node.getInt(getNodePath("IdleConnectionTestPeriod"), 9);
+        return node.getInt(getNodePath("IdleConnectionTestPeriod"), config.getInt("rds.IdleConnectionTestPeriod", 9));
     }
 
     private String getNodePath(String key) {
-        return String.format("%s/%s", "mysql", key);
+        return String.format("mysql/%s%s", "".equals(savleFlag) ? "" : "slave/", key);
+    }
+
+    public boolean isConfigNull() {
+        return Utils.isEmpty(site()) || Utils.isEmpty(database()) || Utils.isEmpty(serverTimezone());
+    }
+
+    public String getConnectUrl() {
+        if (isConfigNull())
+            throw new RuntimeException("mysql connection config is null");
+
+        return String.format(
+                "jdbc:mysql://%s/%s?useSSL=false&autoReconnect=true&autoCommit=false&useUnicode=true&characterEncoding=utf8&serverTimezone=%s",
+                site(), database(), serverTimezone());
+    }
+
+    public final ComboPooledDataSource createDataSource() {
+        log.info("create pool to: " + site());
+        // 使用线程池创建
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass(ZkMysqlConfig.JdbcDriver);
+        } catch (PropertyVetoException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+        var host = site();
+        var database = database();
+        var timezone = serverTimezone();
+        if (Utils.isEmpty(host) || Utils.isEmpty(database) || Utils.isEmpty(timezone))
+            throw new RuntimeException("mysql connection config is null");
+
+        var jdbcUrl = String.format(
+                "jdbc:mysql://%s/%s?useSSL=false&autoReconnect=true&autoCommit=false&useUnicode=true&characterEncoding=utf8&serverTimezone=%s",
+                host, database, timezone);
+
+        dataSource.setJdbcUrl(jdbcUrl);
+        dataSource.setUser(username());
+        dataSource.setPassword(password());
+        // 连接池大小设置
+        dataSource.setMaxPoolSize(maxPoolSize());
+        dataSource.setMinPoolSize(minPoolSize());
+        dataSource.setInitialPoolSize(initialPoolSize());
+        // 连接池断开控制
+        dataSource.setCheckoutTimeout(checkoutTimeout()); // 单位毫秒
+        dataSource.setMaxIdleTime(maxIdleTime()); // 空闲自动断开时间
+        // 每隔多少时间（时间请小于 数据库的 timeout）,测试一下链接，防止失效，会损失小部分性能
+        dataSource.setIdleConnectionTestPeriod(idleConnectionTestPeriod()); // 单位秒
+        dataSource.setTestConnectionOnCheckin(true);
+        dataSource.setTestConnectionOnCheckout(false);
+        return dataSource;
+    }
+
+    public Connection createConnection() {
+        var host = site();
+        var database = database();
+        var timezone = serverTimezone();
+        if (Utils.isEmpty(host) || Utils.isEmpty(database) || Utils.isEmpty(timezone))
+            throw new RuntimeException("mysql connection config is null");
+        var jdbcUrl = String.format(
+                "jdbc:mysql://%s/%s?useSSL=false&autoReconnect=true&autoCommit=false&useUnicode=true&characterEncoding=utf8&serverTimezone=%s",
+                host, database, timezone);
+        try {
+            Class.forName(ZkMysqlConfig.JdbcDriver);
+            return DriverManager.getConnection(jdbcUrl, username(), password());
+        } catch (SQLException | ClassNotFoundException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+
     }
 
 }
