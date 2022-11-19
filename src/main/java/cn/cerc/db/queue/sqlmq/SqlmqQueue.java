b95028dfc3acc69270f810649a2481550f2cd193
mimrc-LiYuan <460289628@qq.com>
2022-11-19 11:04:59 +0800

    增加多消费者redis锁功能

--- src/main/java/cn/cerc/db/queue/sqlmq/SqlmqQueue.java
+++ src/main/java/cn/cerc/db/queue/sqlmq/SqlmqQueue.java
@@ -10,6 +10,7 @@ import cn.cerc.db.core.Datetime.DateType;
 import cn.cerc.db.core.IHandle;
 import cn.cerc.db.mysql.MysqlQuery;
 import cn.cerc.db.queue.OnStringMessage;
+import cn.cerc.db.redis.Redis;
 
 public class SqlmqQueue {
     private static final Logger log = LoggerFactory.getLogger(SqlmqQueue.class);
@@ -45,34 +46,44 @@ public class SqlmqQueue {
         query.add("or (status_=%d and show_time_ <= '%s'))", StatusEnum.Next.ordinal(), new Datetime());
         query.setMaximum(1);
         query.open();
-        for (var row : query) {
-            boolean result = false;
-            String content = "";
-            var uid = row.bind("UID_");
-            try {
-                addLog(uid.getLong(), AckEnum.Read, content);
-                query.edit();
-                query.setValue("status_", StatusEnum.Working.ordinal());
-                query.setValue("consume_times_", query.getInt("consume_times_") + 1);
-                query.setValue("version_", query.getInt("version_") + 1);
-                query.post();
-                result = onConsume.consume(row.getString("message_"));
-            } catch (Exception e) {
-                log.error(e.getMessage(), e);
-                content = e.getMessage();
-            }
-            addLog(uid.getLong(), result ? AckEnum.Ok : AckEnum.Error, content);
+        try (Redis redis = new Redis()) {
+            for (var row : query) {
+                var uid = row.bind("UID_");
+                var lockKey = "sqlmq." + uid.getString();
+                if (redis.setnx(lockKey, new Datetime().toString()) == 0)
+                    continue;
+                redis.expire(lockKey, 60 * 30);
+                try {
+                    String content = "";
+                    boolean result = false;
+                    try {
+                        addLog(uid.getLong(), AckEnum.Read, content);
+                        query.edit();
+                        query.setValue("status_", StatusEnum.Working.ordinal());
+                        query.setValue("consume_times_", query.getInt("consume_times_") + 1);
+                        query.setValue("version_", query.getInt("version_") + 1);
+                        query.post();
+                        result = onConsume.consume(row.getString("message_"));
+                    } catch (Exception e) {
+                        log.error(e.getMessage(), e);
+                        content = e.getMessage();
+                    }
+                    addLog(uid.getLong(), result ? AckEnum.Ok : AckEnum.Error, content);
 
-            if (result) {
-                query.edit();
-                query.setValue("status_", StatusEnum.Finish.ordinal());
-            } else {
-                query.edit();
-                query.setValue("status_", StatusEnum.Next.ordinal());
-                query.setValue("show_time_", new Datetime().inc(DateType.Minute, 30));
+                    if (result) {
+                        query.edit();
+                        query.setValue("status_", StatusEnum.Finish.ordinal());
+                    } else {
+                        query.edit();
+                        query.setValue("status_", StatusEnum.Next.ordinal());
+                        query.setValue("show_time_", new Datetime().inc(DateType.Minute, 30));
+                    }
+                    query.setValue("version_", query.getInt("version_") + 1);
+                    query.post();
+                } finally {
+                    redis.del(lockKey);
+                }
             }
-            query.setValue("version_", query.getInt("version_") + 1);
-            query.post();
         }
     }
 
