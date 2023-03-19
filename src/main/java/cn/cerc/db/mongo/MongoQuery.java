e725b75b0a9d315444da5c0a415a2f58f206bb83
张弓 <sz9214e@qq.com>
2023-03-19 11:51:33 +0800

    Update MongoQuery.java

--- src/main/java/cn/cerc/db/mongo/MongoQuery.java
+++ src/main/java/cn/cerc/db/mongo/MongoQuery.java
@@ -70,7 +70,7 @@ public class MongoQuery extends DataSet implements IHandle {
             return this;
 
         for (Document doc : list) {
-            DataRow record = append().current();
+            DataRow record = append().currentRow().get();
             for (String field : doc.keySet()) {
                 if ("_id".equals(field)) {
                     Object uid = doc.get(field);
@@ -276,7 +276,7 @@ public class MongoQuery extends DataSet implements IHandle {
         List<Map<String, Object>> items = (List<Map<String, Object>>) value;
         DataSet dataSet = new DataSet();
         for (Map<String, Object> item : items) {
-            DataRow record = dataSet.append().current();
+            DataRow record = dataSet.append().currentRow().get();
             for (String key : item.keySet()) {
                 record.setValue(key, item.get(key));
             }
