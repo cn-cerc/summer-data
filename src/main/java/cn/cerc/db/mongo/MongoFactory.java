50b0442ebdb4502d6936738b1bfeb7e4837b42d8
HUANG Rongjun <l1091462907@gmail.com>
2023-05-11 11:09:51 +0800

    add

--- src/main/java/cn/cerc/db/mongo/MongoFactory.java
+++ src/main/java/cn/cerc/db/mongo/MongoFactory.java
@@ -1,6 +1,5 @@
 package cn.cerc.db.mongo;
 
-import org.bson.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -8,28 +7,26 @@ import org.springframework.stereotype.Component;
 import com.mongodb.ConnectionString;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
 
 import cn.cerc.db.core.ServerConfig;
 import cn.cerc.db.core.Utils;
 import cn.cerc.db.zk.ZkNode;
 
 @Component
-public class MongoConfig {
-    private static final Logger log = LoggerFactory.getLogger(MongoConfig.class);
+public class MongoFactory {
+    private static final Logger log = LoggerFactory.getLogger(MongoFactory.class);
 
     private static final ServerConfig config = ServerConfig.getInstance();
 
     private static final String prefix = String.format("/%s/%s/mongodb/", ServerConfig.getAppProduct(),
             ServerConfig.getAppVersion());
 
-    public static MongoClient getClient() {
+    public static MongoClient getUrl() {
         var username = ZkNode.get()
                 .getNodeValue(prefix + "username", () -> config.getProperty("mgdb.username", "mongodb_user"));
         var password = ZkNode.get()
                 .getNodeValue(prefix + "password", () -> config.getProperty("mgdb.password", "mongodb_password"));
-        var database = MongoConfig.database();
+        var database = MongoFactory.database();
         var maxpoolsize = ZkNode.get()
                 .getNodeValue(prefix + "maxpoolsize", () -> config.getProperty("mgdb.maxpoolsize", "100"));// 单客户端默认最大100个连接
         var hosts = ZkNode.get()
@@ -65,10 +62,5 @@ public class MongoConfig {
         return database;
     }
 
-    public static MongoCollection<Document> getCollection(String collectionName) {
-        MongoClient mongoClient = MongoConfig.getClient();
-        MongoDatabase database = mongoClient.getDatabase(MongoConfig.database());
-        return database.getCollection(collectionName);
-    }
 
 }
