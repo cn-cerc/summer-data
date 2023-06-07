a25fbfdc92c92fc2d621acd9bc258acfca885282
HUANG Rongjun <l1091462907@gmail.com>
2023-06-07 18:07:07 +0800

    rabbitmq的 connection 改为使用连接池处理，默认使用轮询的方式

---
+++ src/main/java/cn/cerc/db/queue/rabbitmq/RabbitFactory.java
@@ -0,0 +1,64 @@
+package cn.cerc.db.queue.rabbitmq;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import cn.cerc.db.core.ServerConfig;
+import cn.cerc.db.zk.ZkNode;
+
+/**
+ * rabbit 连接工厂类
+ */
+public enum RabbitFactory {
+
+    instance;
+
+    private static final Logger log = LoggerFactory.getLogger(RabbitFactory.class);
+    private ConnectionFactory factory;
+
+    public static RabbitFactory getInstance() {
+        return instance;
+    }
+
+    public ConnectionFactory getFactory() {
+        if (factory != null)
+            return factory;
+
+        synchronized (RabbitFactory.class) {
+            if (factory == null) {
+                final String prefix = String.format("/%s/%s/rabbitmq/", ServerConfig.getAppProduct(),
+                        ServerConfig.getAppVersion());
+                var host = ZkNode.get().getNodeValue(prefix + "host", () -> "rabbitmq.local.top");
+                var port = ZkNode.get().getNodeValue(prefix + "port", () -> "5672");
+                var username = ZkNode.get().getNodeValue(prefix + "username", () -> "admin");
+                var password = ZkNode.get().getNodeValue(prefix + "password", () -> "admin");
+
+                // 创建连接工厂
+                factory = new ConnectionFactory();
+                factory.setHost(host);
+                factory.setPort(Integer.parseInt(port));
+                factory.setUsername(username);
+                factory.setPassword(password);
+                factory.setConnectionTimeout(30000);
+                factory.setRequestedHeartbeat(60);
+            }
+        }
+        return factory;
+    }
+
+    public Connection newConnection() throws IOException, TimeoutException {
+        Connection connection = RabbitFactory.getInstance().getFactory().newConnection();
+        if (connection == null)
+            throw new RuntimeException("rabbitmq connection 创建失败，请立即检查 RabbitMQ 的服务状态");
+        connection.addShutdownListener(
+                cause -> log.debug("{}:{} rabbitmq connection closed", factory.getHost(), factory.getPort()));
+        return connection;
+    }
+
+}
--- src/main/java/cn/cerc/db/queue/rabbitmq/RabbitServer.java
+++ src/main/java/cn/cerc/db/queue/rabbitmq/RabbitServer.java
@@ -1,6 +1,8 @@
 package cn.cerc.db.queue.rabbitmq;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -8,87 +10,74 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
 
-import cn.cerc.db.core.ServerConfig;
-import cn.cerc.db.zk.ZkNode;
-
-public enum RabbitServer {
-    INSTANCE;
-
+public class RabbitServer {
     private static final Logger log = LoggerFactory.getLogger(RabbitServer.class);
-    private final AtomicInteger atomic = new AtomicInteger();
+    private static final int processors = Runtime.getRuntime().availableProcessors();
+    private static final List<Connection> connections = new ArrayList<>(processors);
+    private static final AtomicInteger loader = new AtomicInteger();
 
-    private ConnectionFactory factory;
-    private Connection connection;
+    private static final RabbitServer instance = new RabbitServer();
 
     public static RabbitServer getInstance() {
-        return INSTANCE;
+        return instance;
     }
 
-    public ConnectionFactory getFactory() {
-        if (factory != null)
-            return factory;
-
-        synchronized (RabbitServer.class) {
-            if (factory == null) {
-                final String prefix = String.format("/%s/%s/rabbitmq/", ServerConfig.getAppProduct(),
-                        ServerConfig.getAppVersion());
-                var host = ZkNode.get().getNodeValue(prefix + "host", () -> "rabbitmq.local.top");
-                var port = ZkNode.get().getNodeValue(prefix + "port", () -> "5672");
-                var username = ZkNode.get().getNodeValue(prefix + "username", () -> "admin");
-                var password = ZkNode.get().getNodeValue(prefix + "password", () -> "admin");
-
-                // 创建连接工厂
-                factory = new ConnectionFactory();
-                factory.setHost(host);
-                factory.setPort(Integer.parseInt(port));
-                factory.setUsername(username);
-                factory.setPassword(password);
-                factory.setConnectionTimeout(30000);
-                factory.setRequestedHeartbeat(60);
+    private RabbitServer() {
+        for (int i = 0; i < processors; i++) {
+            try {
+                Connection connection = RabbitFactory.getInstance().newConnection();
+                connections.add(connection);
+            } catch (IOException | TimeoutException e) {
+                log.error(e.getMessage(), e);
             }
         }
-        return factory;
     }
 
+    /**
+     * 获取连接，自动重连
+     * 
+     * @return connection
+     */
     public Connection getConnection() {
-        if (connection != null)
-            return connection;
-
-        synchronized (RabbitServer.class) {
-            if (connection == null) {
-                ConnectionFactory factory = RabbitServer.getInstance().getFactory();
-                if (this.atomic.get() >= 3) {
-                    log.error("{}:{} rabbitmq 尝试连接 {} 次失败，不再进行尝试", factory.getHost(), factory.getPort(),
-                            this.atomic.get());
-                    return null;
-                }
-
-                try {
-                    connection = factory.newConnection();
-                    if (connection == null)
-                        throw new RuntimeException("rabbitmq connection 创建失败，请立即检查 mq 的服务状态");
-                    connection.addShutdownListener(cause -> log.debug("{}:{} rabbitmq connection closed",
-                            factory.getHost(), factory.getPort()));
-                } catch (IOException | TimeoutException e) {
-                    if (this.atomic.get() < 3) {
-                        log.error("{}:{} {}", factory.getHost(), factory.getPort(), e.getMessage(), e);
-                        this.atomic.incrementAndGet();
-                    }
-                }
+        int index = next();
+        Connection connection = connections.get(index);
+        try {
+            if (connection == null || !connection.isOpen()) {
+                connection = RabbitFactory.getInstance().newConnection();
+                connections.set(index, connection);
             }
+        } catch (IOException | TimeoutException e) {
+            log.error("{} 连接池游标异常 -> {}", e.getMessage(), e);
         }
         return connection;
-
     }
 
+    /**
+     * 获取下一个连接游标
+     * 
+     * @return index 负载游标
+     */
+    private int next() {
+        int index = loader.getAndIncrement();
+        if (loader.get() > processors) {
+            loader.set(0);
+            index = 0;
+        }
+        return index;
+    }
+
+    /**
+     * 关闭连接
+     */
     public void close() {
-        if (connection != null) {
-            try {
-                connection.close();
-            } catch (Exception e) {
-                log.error(e.getMessage(), e);
+        for (Connection connection : connections) {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (Exception e) {
+                    log.error(e.getMessage(), e);
+                }
             }
             connection = null;
         }
