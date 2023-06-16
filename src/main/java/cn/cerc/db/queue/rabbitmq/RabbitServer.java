fb5a8d7caa09d9cfde7b4a63e7501a6abd7ab158
HUANG Rongjun <l1091462907@gmail.com>
2023-06-16 11:35:04 +0800

    Update RabbitServer.java

--- src/main/java/cn/cerc/db/queue/rabbitmq/RabbitServer.java
+++ src/main/java/cn/cerc/db/queue/rabbitmq/RabbitServer.java
@@ -13,7 +13,6 @@ import com.rabbitmq.client.ConnectionFactory;
 
 /**
  * rabbitMQ 连接管理
- *
  */
 public class RabbitServer {
     private static final Logger log = LoggerFactory.getLogger(RabbitServer.class);
@@ -45,9 +44,9 @@ public class RabbitServer {
 
     /**
      * 获取连接，自动重连
-     *
+     * <p>
      * 从连接池中获取连接，如果池为空则阻塞等待
-     * 
+     *
      * @return connection
      * @throws InterruptedException
      */
@@ -60,7 +59,7 @@ public class RabbitServer {
 
     /**
      * 将连接放回连接池
-     * 
+     *
      * @param connection
      */
     public void releaseConnection(Connection connection) {
@@ -72,6 +71,7 @@ public class RabbitServer {
      * 关闭所有连接
      */
     public void close() throws IOException {
+        log.info("关闭连接，剩余个数 {}", connections.size());
         for (Connection connection : connections) {
             connection.close();
         }
