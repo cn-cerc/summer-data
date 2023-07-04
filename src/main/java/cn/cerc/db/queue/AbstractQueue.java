6a6beef82caddf81e75804288013a1900491cc23
胡杰 <h2syj.zzzz@gmail.com>
2023-07-04 13:25:15 +0000

    !266 修改SqlmqQueue让他支持顺序消费

--- src/main/java/cn/cerc/db/queue/AbstractQueue.java
+++ src/main/java/cn/cerc/db/queue/AbstractQueue.java
@@ -1,6 +1,7 @@
 package cn.cerc.db.queue;
 
 import java.util.Objects;
+import java.util.Optional;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -12,7 +13,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import cn.cerc.db.core.Datetime;
+import cn.cerc.db.core.Datetime.DateType;
 import cn.cerc.db.core.ServerConfig;
+import cn.cerc.db.core.Utils;
 import cn.cerc.db.queue.mns.MnsServer;
 import cn.cerc.db.queue.rabbitmq.RabbitQueue;
 import cn.cerc.db.queue.sqlmq.SqlmqQueue;
@@ -42,9 +46,12 @@ public abstract class AbstractQueue implements OnStringMessage, Watcher, Runnabl
     private static ZkConfig config;
     private boolean pushMode = false; // 默认为拉模式
     private QueueServiceEnum service;
-    private int delayTime = 60; // 单位：秒
+    private int delayTime = 60; // 失败重试时间 单位：秒
+    private Optional<Datetime> showTime = Optional.empty(); // 队列延时时间 默认当前时间
     private String original;
     private String order;
+    private String groupCode;
+    private int priorId;
 
     public AbstractQueue() {
         super();
@@ -91,6 +98,19 @@ public abstract class AbstractQueue implements OnStringMessage, Watcher, Runnabl
     // 创建延迟队列消息
     public final long getDelayTime() {
         return this.delayTime;
+    }
+
+    /**
+     * 不要在单例模式下使用该方法推送延时消息！单例模式下showTime可能会被其他线程更改！
+     * 
+     * @param showTime 设置延迟时间
+     */
+    protected void setShowTime(Datetime showTime) {
+        this.showTime = Optional.ofNullable(showTime);
+    }
+
+    public final Optional<Datetime> getShowTime() {
+        return this.showTime;
     }
 
     public void startService() {
@@ -149,9 +169,15 @@ public abstract class AbstractQueue implements OnStringMessage, Watcher, Runnabl
         case Sqlmq -> {
             SqlmqQueue sqlQueue = SqlmqServer.getQueue(this.getId());
             sqlQueue.setDelayTime(delayTime);
+            sqlQueue.setShowTime(showTime.orElseGet(Datetime::new));
             sqlQueue.setService(service);
             sqlQueue.setQueueClass(this.getClass().getSimpleName());
-            return sqlQueue.push(data, this.order);
+            String messageId = sqlQueue.push(data, this.order, this.groupCode, this.priorId);
+            if (!Utils.isEmpty(this.groupCode)) {
+                this.setPriorId(Integer.parseInt(messageId));
+                this.setShowTime(new Datetime().inc(DateType.Year, 1));
+            }
+            return messageId;
         }
         case RabbitMQ -> {
             try (RabbitQueue queue = new RabbitQueue(this.getId())) {
@@ -270,6 +296,22 @@ public abstract class AbstractQueue implements OnStringMessage, Watcher, Runnabl
             log.warn("已完成的任务数量 {}", executor.getCompletedTaskCount());
             log.warn("累计的总任务数量 {}", executor.getTaskCount());
         }
+    }
+
+    public String getGroupCode() {
+        return groupCode;
+    }
+
+    public void setGroupCode(String groupCode) {
+        this.groupCode = groupCode;
+    }
+
+    public int getPriorId() {
+        return priorId;
+    }
+
+    public void setPriorId(int priorId) {
+        this.priorId = priorId;
     }
 
 }
