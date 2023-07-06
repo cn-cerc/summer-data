1753af2d4c5f622df2206399e4e555f21344d339
胡杰 <h2syj.zzzz@gmail.com>
2023-07-06 20:34:04 +0800

    Update SqlmqQueue.java

--- src/main/java/cn/cerc/db/queue/sqlmq/SqlmqQueue.java
+++ src/main/java/cn/cerc/db/queue/sqlmq/SqlmqQueue.java
@@ -93,7 +93,7 @@ public class SqlmqQueue implements IHandle {
         var lockKey = "sqlmq." + uid.getString();
         if (redis.setnx(lockKey, new Datetime().toString()) == 0)
             return;
-        redis.expire(lockKey, 60 * 30);
+        redis.expire(lockKey, delayTime + 5);
         try {
             String content = "";
             boolean result = false;
