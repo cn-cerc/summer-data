254655068966454c94c4d9e54f658bcce0de8bf8
HUANG Rongjun <l1091462907@gmail.com>
2023-07-14 22:47:38 +0800

    Update MysqlServer.java

--- src/main/java/cn/cerc/db/mysql/MysqlServer.java
+++ src/main/java/cn/cerc/db/mysql/MysqlServer.java
@@ -15,8 +15,8 @@ import org.springframework.stereotype.Component;
 import com.zaxxer.hikari.HikariDataSource;
 
 import cn.cerc.db.core.IHandle;
-import cn.cerc.db.core.ServerClient;
 import cn.cerc.db.core.ISqlServer;
+import cn.cerc.db.core.ServerClient;
 
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
@@ -36,12 +36,8 @@ public abstract class MysqlServer implements ISqlServer, AutoCloseable {
 
     @Override
     public final MysqlClient getClient() {
-        if (client == null) {
-            synchronized (this) {
-                if (client == null)
-                    client = new MysqlClient(this, this.isPool());
-            }
-        }
+        if (client == null)
+            client = new MysqlClient(this, this.isPool());
         return client.incReferenced();
     }
 
