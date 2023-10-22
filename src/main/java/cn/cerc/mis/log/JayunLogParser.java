eb5ebca3a72aa1941306c944153ca989fad96257
HUANG Rongjun <l1091462907@gmail.com>
2023-10-22 17:13:20 +0800

    简化日志解析器

--- src/main/java/cn/cerc/mis/log/JayunLogParser.java
+++ src/main/java/cn/cerc/mis/log/JayunLogParser.java
@@ -1,7 +1,9 @@
 package cn.cerc.mis.log;
 
 import java.util.Enumeration;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -19,6 +21,18 @@ import cn.cerc.db.core.Utils;
 public class JayunLogParser {
     private static volatile JayunLogParser instance;
     private final String loggerName;
+
+    /**
+     * 通缉名单
+     */
+    private static final Set<String> wanted = new HashSet<>();
+    static {
+        wanted.add("site.diteng");
+        wanted.add("site.obm");
+        wanted.add("site.oem");
+        wanted.add("site.odm");
+        wanted.add("site.fpl");
+    }
 
     private JayunLogParser() {
         String loggerName = "";
@@ -55,19 +69,18 @@ public class JayunLogParser {
     /**
      * 警告类日志
      */
-    public static void warn(Class<?> clazz, LastModified modified, Throwable throwable, String message) {
-        JayunLogParser.analyze(clazz, modified, throwable, message, JayunLogData.warn);
+    public static void warn(Class<?> clazz, Throwable throwable) {
+        JayunLogParser.analyze(clazz, throwable, JayunLogData.warn);
     }
 
     /**
      * 错误类日志
      */
-    public static void error(Class<?> clazz, LastModified modified, Throwable throwable, String message) {
-        JayunLogParser.analyze(clazz, modified, throwable, message, JayunLogData.error);
+    public static void error(Class<?> clazz, Throwable throwable) {
+        JayunLogParser.analyze(clazz, throwable, JayunLogData.error);
     }
 
-    private static void analyze(Class<?> clazz, LastModified modified, Throwable throwable, String message,
-            String level) {
+    private static void analyze(Class<?> clazz, Throwable throwable, String level) {
         if (throwable == null)
             return;
 
@@ -78,36 +91,46 @@ public class JayunLogParser {
         if (Utils.isEmpty(JayunLogParser.loggerName()))
             return;
 
+        String origin = clazz.getName();
+        String message = throwable.getMessage();
         JayunLogData data = new JayunLogData();
-        data.setId(clazz.getName());
+        data.setId(origin);
         data.setLine("?");
         data.setLevel(level);
         data.setMessage(message);
 
-        String[] stack = DefaultThrowableRenderer.render(throwable);
-        for (String line : stack) {
-            // 如果捕捉到业务代码就重置触发器信息
-            if (line.contains("site.diteng")) {
-                line = line.trim();
-                String trigger = JayunLogParser.trigger(line);
-                if (Utils.isEmpty(trigger))
-                    continue;
-                data.setId(trigger);
-                data.setLine(JayunLogParser.lineNumber(line));
-
-                try {
-                    Class<?> caller = Class.forName(trigger);
-                    modified = caller.getAnnotation(LastModified.class);
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
-                }
-                break;
-            }
-        }
-
+        LastModified modified = clazz.getAnnotation(LastModified.class);
         if (modified != null) {
             data.setName(modified.name());
             data.setDate(modified.date());
+        }
+
+        String[] stack = DefaultThrowableRenderer.render(throwable);
+        // 起源类没有在通缉名单上再抓堆栈信息
+        if (wanted.stream().noneMatch(origin::contains)) {
+            for (String line : stack) {
+                // 如果捕捉到业务代码就重置触发器信息
+                if (wanted.stream().anyMatch(origin::contains)) {
+                    line = line.trim();
+                    String trigger = JayunLogParser.trigger(line);
+                    if (Utils.isEmpty(trigger))
+                        continue;
+                    data.setId(trigger);
+                    data.setLine(JayunLogParser.lineNumber(line));
+
+                    try {
+                        Class<?> caller = Class.forName(trigger);
+                        modified = caller.getAnnotation(LastModified.class);
+                        if (modified != null) {
+                            data.setName(modified.name());
+                            data.setDate(modified.date());
+                        }
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                }
+            }
         }
 
         data.setStack(stack);
