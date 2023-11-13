7aef06df499873a7a4996f08387e3bbb0e87735f
封威 <1876381677@qq.com>
2023-11-13 19:54:20 +0800

    Update Locker.java

--- src/main/java/cn/cerc/db/redis/Locker.java
+++ src/main/java/cn/cerc/db/redis/Locker.java
@@ -18,6 +18,7 @@ public class Locker implements Closeable {
     private String group;
     private String message;
     private Map<String, Boolean> items = new HashMap<>();
+    private int timeout = 10000; // 锁超时时间，默认10秒
 
     public Locker(String group, Object first, Object... args) {
         this.group = group;
@@ -25,15 +26,22 @@ public class Locker implements Closeable {
         for (Object arg : args)
             items.put(group + "-" + arg, false);
     }
-
-    public boolean lock(String flag, int timeout) {
-        if (timeout % 100 != 0)
-            throw new RuntimeException(String.format("%s %% 100 !=0", timeout));
+    
+    /**
+     * 默认三秒释放锁
+     */
+    public boolean lock() {
+        return lock("", 3000);
+    }
+    
+    public boolean lock(String flag, int time) {
+        if (time % 100 != 0)
+            throw new RuntimeException(String.format("%s %% 100 !=0", time));
         if (items.size() == 0)
             items.put(group, false);
         try (Jedis jedis = JedisFactory.getJedis()) {
             for (String key : items.keySet()) {
-                if (!tryLock(jedis, key, flag, timeout)) {
+                if (!tryLock(jedis, key, flag, time)) {
                     log.warn(this.message);
                     return false;
                 }
@@ -46,15 +54,15 @@ public class Locker implements Closeable {
         }
     }
 
-    private boolean tryLock(Jedis jedis, String key, String flag, int timeout) throws InterruptedException {
+    private boolean tryLock(Jedis jedis, String key, String flag, int time) throws InterruptedException {
         int i = 0;
         int totalSleepTime = 0;
         // 重试机制 递增睡眠时间
-        while (totalSleepTime < timeout) {
+        while (totalSleepTime < time) {
             if (jedis.setnx(key, System.currentTimeMillis() + "," + flag) == 1) {
                 this.message = String.format(res.getString(1, "[%s]%s锁定成功"), key, flag);
                 // 设置到期时间，避免服务器更新时，为能正常释放key，导致死锁
-                jedis.expire(key, timeout);
+                jedis.expire(key, this.timeout);
                 return true;
             }
             int sleepTime = i++ * 100;
@@ -73,6 +81,20 @@ public class Locker implements Closeable {
                     jedis.del(key);
             }
         }
+    }
+
+    public Locker add(Object key) {
+        items.put(group + "-" + key, false);
+        return this;
+    }
+
+    public int getTimeout() {
+        return timeout;
+    }
+
+    public Locker setTimeout(int timeout) {
+        this.timeout = timeout;
+        return this;
     }
 
     public String getMessage() {
