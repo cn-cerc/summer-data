00b980d201886a1fb843e9fc8c85ed92111677bf
封威 <1876381677@qq.com>
2023-11-13 18:07:14 +0800

    update

--- src/main/java/cn/cerc/db/redis/Locker.java
+++ src/main/java/cn/cerc/db/redis/Locker.java
@@ -9,7 +9,6 @@ import org.slf4j.LoggerFactory;
 
 import cn.cerc.db.SummerDB;
 import cn.cerc.db.core.ClassResource;
-import cn.cerc.db.core.Datetime;
 import redis.clients.jedis.Jedis;
 
 public class Locker implements Closeable {
@@ -19,37 +18,22 @@ public class Locker implements Closeable {
     private String group;
     private String message;
     private Map<String, Boolean> items = new HashMap<>();
-    private int timeout = 10000; // 锁超时时间，默认10秒
 
     public Locker(String group, Object first, Object... args) {
         this.group = group;
         items.put(group + "-" + first, false);
-        for (Object arg : args) {
+        for (Object arg : args)
             items.put(group + "-" + arg, false);
-        }
     }
 
-    @Deprecated
-    public void add(Object key) {
-        items.put(group + "-" + key, false);
-    }
-
-    @Deprecated
-    public boolean lock(String flag) {
-        return lock(flag, 100);
-    }
-
-    public boolean lock(String flag, int time) {
-        if (time % 100 != 0) {
-            throw new RuntimeException(String.format("%s %% 100 !=0", time));
-        }
-        if (items.size() == 0) {
+    public boolean lock(String flag, int timeout) {
+        if (timeout % 100 != 0)
+            throw new RuntimeException(String.format("%s %% 100 !=0", timeout));
+        if (items.size() == 0)
             items.put(group, false);
-        }
         try (Jedis jedis = JedisFactory.getJedis()) {
             for (String key : items.keySet()) {
-                // System.out.println("key: " + key);
-                if (!tryLock(jedis, key, flag, time / 100)) {
+                if (!tryLock(jedis, key, flag, timeout)) {
                     log.warn(this.message);
                     return false;
                 }
@@ -62,69 +46,37 @@ public class Locker implements Closeable {
         }
     }
 
-    private boolean tryLock(Jedis jedis, String key, String flag, int num) throws InterruptedException {
-        boolean result = false;
+    private boolean tryLock(Jedis jedis, String key, String flag, int timeout) throws InterruptedException {
         int i = 0;
-        while (i < num) {
-            i++;
-            long curTime = System.currentTimeMillis() + timeout;
-            if (jedis.setnx(key, curTime + "," + flag) == 1) {
+        int totalSleepTime = 0;
+        // 重试机制
+        while (totalSleepTime < timeout) {
+            if (jedis.setnx(key, System.currentTimeMillis() + "," + flag) == 1) {
                 this.message = String.format(res.getString(1, "[%s]%s锁定成功"), key, flag);
-                result = true;
-                break;
-            } else {
-                String currentValue = jedis.get(key);
-                if (currentValue != null && currentValue.split(",").length == 2) {
-                    String[] args = currentValue.split(",");
-                    long lastTime = Long.parseLong(args[0]);
-                    if (System.currentTimeMillis() > lastTime) {
-                        String oldValue = jedis.getSet(key, curTime + "," + flag);
-                        if (oldValue != null && oldValue.equals(currentValue)) {
-                            // System.out.println("lastTime: " + lastTime);
-                            // System.out.println(" curTime: " + curTime);
-                            this.message = String.format(res.getString(2, "[%s]%s强制锁定成功"), key, flag);
-                            // System.out.println(this.message);
-                            result = true;
-                            break;
-                        }
-                    }
-                    Datetime tmp = new Datetime(lastTime);
-                    this.message = String.format(res.getString(3, "[%s]%s锁定失败， %s完成后(%s)再试"), key, flag, args[1],
-                            tmp.getTime());
-                } else {
-                    this.message = String.format(res.getString(4, "[%s]%s锁定失败， %s完成后再试"), key, flag, currentValue);
-                }
+                // 设置到期时间，避免服务器更新时，为能正常释放key，导致死锁
+                jedis.expire(key, timeout);
+                return true;
             }
-            if (i < num) {
-                // System.out.println("try " + (i + 1));
-                Thread.sleep(100);
-            }
+            int sleepTime = i++ * 100;
+            totalSleepTime += sleepTime;
+            Thread.sleep(sleepTime);
         }
-        log.debug(this.message);
-        return result;
+        this.message = String.format(res.getString(4, "[%s]%s锁定失败"), key, flag);
+        return false;
     }
 
     @Override
     public void close() {
         try (Jedis jedis = JedisFactory.getJedis()) {
             for (String key : items.keySet()) {
-                if (items.get(key)) {
+                if (items.get(key))
                     jedis.del(key);
-                }
             }
         }
     }
 
     public String getMessage() {
         return message;
-    }
-
-    public int getTimeout() {
-        return timeout;
-    }
-
-    public void setTimeout(int timeout) {
-        this.timeout = timeout;
     }
 
 }
---
+++ src/test/java/cn/cerc/db/redis/LockerTest.java
@@ -0,0 +1,151 @@
+package cn.cerc.db.redis;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class LockerTest {
+
+    private final String group = LockerTest.class.getSimpleName() + ".group";
+    private final String firstKey = LockerTest.class.getSimpleName() + ".key";
+    private final int count = 10000;
+
+    @Test
+    public void lock_test1() {
+        try (Locker lock = new Locker(group, firstKey)) {
+            assertEquals(lock.lock("标记", 1000), true);
+            assertEquals(lock.lock("标记", 1000), false);
+        }
+    }
+
+    @Test
+    public void lock_test2() {
+        try (Locker lock = new Locker(group, firstKey)) {
+            assertEquals(lock.lock("标记", 1000), true);
+        }
+        try (Locker lock = new Locker(group, firstKey)) {
+            assertEquals(lock.lock("标记", 1000), true);
+        }
+    }
+
+    @Test
+    public void lockTransferMoney() throws InterruptedException {
+        BankAccount account1 = new BankAccount(1000);
+        BankAccount account2 = new BankAccount(1000);
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < count; i++) {
+                try (Locker lock = new Locker(group, firstKey)) {
+                    if (lock.lock("标记", 1000))
+                        account1.transfer(account2, 10);
+                    else
+                        i--;
+                }
+            }
+        });
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < count; i++) {
+                try (Locker lock = new Locker(group, firstKey)) {
+                    if (lock.lock("标记", 1000))
+                        account2.transfer(account1, 10);
+                    else
+                        i--;
+                }
+            }
+        });
+        Thread t3 = new Thread(() -> {
+            for (int i = 0; i < count; i++) {
+                try (Locker lock = new Locker(group, firstKey)) {
+                    if (lock.lock("标记", 1000))
+                        account1.transfer(account2, 10);
+                    else
+                        i--;
+                }
+            }
+        });
+        Thread t4 = new Thread(() -> {
+            for (int i = 0; i < count; i++) {
+                try (Locker lock = new Locker(group, firstKey)) {
+                    if (lock.lock("标记", 1000))
+                        account2.transfer(account1, 10);
+                    else
+                        i--;
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+
+        try {
+            t1.join();
+            t2.join();
+            t3.join();
+            t4.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.err.println("lockTransferMoney Final balance of account1: " + account1.getBalance());
+        System.err.println("lockTransferMoney Final balance of account2: " + account2.getBalance());
+    }
+
+    @Test
+    public void notLockTransferMoney() throws InterruptedException {
+        BankAccount account1 = new BankAccount(1000);
+        BankAccount account2 = new BankAccount(1000);
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < count; i++)
+                account1.transfer(account2, 10);
+        });
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < count; i++)
+                account2.transfer(account1, 10);
+        });
+        Thread t3 = new Thread(() -> {
+            for (int i = 0; i < count; i++)
+                account1.transfer(account2, 10);
+        });
+        Thread t4 = new Thread(() -> {
+            for (int i = 0; i < count; i++)
+                account2.transfer(account1, 10);
+        });
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+
+        try {
+            t1.join();
+            t2.join();
+            t3.join();
+            t4.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.err.println("notLockTransferMoney Final balance of account1: " + account1.getBalance());
+        System.err.println("notLockTransferMoney Final balance of account2: " + account2.getBalance());
+        assertEquals(account1.getBalance() == account2.getBalance(), false);
+    }
+
+    class BankAccount {
+        private int balance;
+
+        public BankAccount(int balance) {
+            this.balance = balance;
+        }
+
+        public int getBalance() {
+            return balance;
+        }
+
+        public void transfer(BankAccount target, int amount) {
+            this.balance -= amount;
+            target.balance += amount;
+        }
+    }
+
+}
