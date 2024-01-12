231c08eaf6947908e853fefe5c41df2f6e60d34d
胡杰 <h2syj.zzzz@gmail.com>
2024-01-12 12:47:17 +0000

    !474 XmlTool.toXml 支持格式化返回

--- src/main/java/cn/cerc/local/tool/XmlTool.java
+++ src/main/java/cn/cerc/local/tool/XmlTool.java
@@ -3,8 +3,9 @@ package cn.cerc.local.tool;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
@@ -13,12 +14,23 @@ import cn.cerc.db.core.Utils;
 public class XmlTool {
     private static final Logger log = LoggerFactory.getLogger(XmlTool.class);
 
-    public static String ROOT_ELEMENT_RESPONSE = "response";
+    private static final XmlMapper mapper = createXmlMapper();
+
+    private XmlTool() {
+    }
+
+    private static XmlMapper createXmlMapper() {
+        return XmlMapper.builder()
+                .defaultUseWrapper(false)
+                .serializationInclusion(Include.NON_NULL) // 字段为 null，自动忽略，不再序列化
+                .enable(MapperFeature.USE_STD_BEAN_NAMING)// 设置转换模式
+                .build();
+    }
 
     /**
      * 对象直接转换为 xml 的字符串
      */
-    public static <T> String toXml(T value) throws JsonProcessingException {
+    public static String toXml(Object value) throws JsonProcessingException {
         return toXml(value, null);
     }
 
@@ -28,14 +40,7 @@ public class XmlTool {
      * @param value 对象实体类
      * @param root  指定的根节点名称
      */
-    public static <T> String toXml(T value, String root) throws JsonProcessingException {
-        XmlMapper mapper = XmlMapper.builder()
-                .defaultUseWrapper(false)
-                .serializationInclusion(JsonInclude.Include.NON_NULL) // 字段为 null，自动忽略，不再序列化
-                // .enable(SerializationFeature.INDENT_OUTPUT) // 格式化输出 xml
-                .enable(MapperFeature.USE_STD_BEAN_NAMING) // 设置转换模式
-                .build();
-
+    public static String toXml(Object value, String root) throws JsonProcessingException {
         String xml;
         if (!Utils.isEmpty(root)) {
             xml = mapper.writer().withRootName(root).writeValueAsString(value);
@@ -43,8 +48,23 @@ public class XmlTool {
             xml = mapper.writeValueAsString(value);
         }
 
-        log.info(xml);
+        log.debug(xml);
         return xml;
+    }
+
+    /**
+     * 格式化xml字符串
+     * 
+     * @param xml  xml字符串
+     * @param root 根节点名称
+     */
+    public static String formatXml(String xml, String root) {
+        try {
+            JsonNode node = mapper.readTree(xml);
+            return mapper.writerWithDefaultPrettyPrinter().withRootName(root).writeValueAsString(node);
+        } catch (JsonProcessingException e) {
+            return xml;
+        }
     }
 
 }
