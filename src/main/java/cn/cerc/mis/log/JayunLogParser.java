49a32d6b1d61c98faaeb1d94ce7e5d663371b42f
胡杰 <h2syj.zzzz@gmail.com>
2024-02-28 09:05:22 +0000

    !506 JayunLog 解析添加 mainName 将责任人发送给 Jayun

--- src/main/java/cn/cerc/mis/log/JayunLogParser.java
+++ src/main/java/cn/cerc/mis/log/JayunLogParser.java
@@ -116,6 +116,7 @@ public class JayunLogParser {
             // 读取起源类修改人
             LastModified modified = clazz.getAnnotation(LastModified.class);
             if (modified != null) {
+                builder.mainName(modified.main());
                 builder.name(modified.name());
                 builder.date(modified.date());
             }
