5e463360a905572f44dbebfaf5ce6f4554d09604
封威 <1876381677@qq.com>
2024-05-11 10:41:43 +0800

    commit

--- src/main/java/cn/cerc/mis/exception/RecordNotExistException.java
+++ src/main/java/cn/cerc/mis/exception/RecordNotExistException.java
@@ -2,13 +2,13 @@ package cn.cerc.mis.exception;
 
 import cn.cerc.db.core.IHandle;
 
-public class SqlOperatorException extends RuntimeException implements IKnowall {
+public class RecordNotExistException extends RuntimeException implements IKnowall {
 
     private static final long serialVersionUID = 1L;
 
     private String[] data;
 
-    public SqlOperatorException(IHandle handle, String message, String dataIn, String sqlText) {
+    public RecordNotExistException(IHandle handle, String message, String dataIn, String sqlText) {
         super(message);
         this.data = new String[] { handle.getCorpNo(), handle.getUserCode(), dataIn, sqlText };
     }
@@ -16,11 +16,6 @@ public class SqlOperatorException extends RuntimeException implements IKnowall {
     @Override
     public String[] getData() {
         return data;
-    }
-
-    @Override
-    public String getGroup() {
-        return IKnowall.记录被其他用户修改;
     }
 
 }
